c4079b78378b82536beed61f89bc6775352a0b15
Marcos <marcos@marcos-Inspiron-1545.(none)>
2013-05-25 18:52:37 -0500

    Autenticación con login y password desde la base de datos.

--- Proyecto/MainWindow.cs
+++ Proyecto/MainWindow.cs
@@ -7,10 +7,34 @@ public partial class MainWindow: Gtk.Window
 	{
 		Build ();
 	}
-	
+
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
 		a.RetVal = true;
 	}
+	
+	protected void OnBtnEntrarClicked (object sender, EventArgs e)
+	{
+		Proyecto.Autenticacion auth = new Proyecto.Autenticacion ();
+		if (auth.valido (this.txtLogin.Text.Trim (), this.txtPassword.Text.Trim ())) {
+			MessageDialog md = new MessageDialog (
+				null, 
+		      	DialogFlags.Modal,
+		  		MessageType.Info, 
+		      	ButtonsType.None, "Bienvenido usuario " + this.txtLogin.Text.Trim ()  
+			);
+			md.Show ();
+		} else {
+			MessageDialog md = new MessageDialog (
+				null, 
+		      	DialogFlags.Modal,
+		  		MessageType.Error, 
+		      	ButtonsType.None, "Usted es un intruso " 
+			);
+			md.Show ();
+		}
+	}
+	
+
 }
